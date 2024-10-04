1889594a9dd67580e94f6fffeb3335cf0fd85904
unknown <renzoocupaaliaga@gmail.com>
2024-10-03 21:01:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const repository = require("./repositorio");
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+
+
+app.get('/api/products', async (req, res) => {
+  res.send(await repository.read());
+});
+
+app.post('/api/pay', async (req, res) => {
+  const ids = req.body;
+
+
+  const productsCopy = await repository.read();
+
+  let error = false;
+
+
+  ids.forEach((id) => {
+    const product = productsCopy.find(p => p.id === id);
+    if (product.stock > 0) {
+      product.stock--;
+    } else {
+      error = true;
+
+    }
+
+
+
+  });
+  if (error) {
+    res.send("sin tock").statusCode(400);
+  }
+  else {
+    await repository.write(productsCopy);
+    products = productsCopy;
+    res.send(productsCopy);
+  }
+
+
+});
+
+
+app.use("/", express.static('fe'));
+
+app.listen(port, () => {
+  console.log(`Example app listening http://localhost:${port}`);
+});
