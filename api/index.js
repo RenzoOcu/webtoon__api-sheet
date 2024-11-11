17f92db59a392e731ef888418735680d81fa44d7
unknown <renzoocupaaliaga@gmail.com>
2024-11-11 11:13:08 -0500

    fix: errors on build

--- api/index.js
+++ api/index.js
@@ -1,26 +1,23 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors'); // Agrega esta línea
-const repository = require("./repositorio.js");
+const repository = require("./repository");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001 ;
 
-app.use(cors()); // Agrega esta línea para habilitar CORS
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/api/products', async (req, res) => {
+app.get("/api/products", async (req, res) => {
   res.send(await repository.read());
 });
 
-app.post('/api/pay', async (req, res) => {
+app.post("/api/pay", async (req, res) => {
   const ids = req.body;
-
   const productsCopy = await repository.read();
-  let error = false;
 
+  let error = false;
   ids.forEach((id) => {
-    const product = productsCopy.find(p => p.id === id);
+    const product = productsCopy.find((p) => p.id === id);
     if (product.stock > 0) {
       product.stock--;
     } else {
@@ -29,14 +26,15 @@ app.post('/api/pay', async (req, res) => {
   });
 
   if (error) {
-    res.status(400).send("sin stock"); // Corrige la línea
-  } else {
+    res.send("Sin stock").statusCode(400);
+  }
+  else {
     await repository.write(productsCopy);
     res.send(productsCopy);
   }
 });
 
-app.use("/", express.static('fe'));
+app.use("/", express.static("public"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
